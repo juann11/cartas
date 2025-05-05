abd433be4eced43398da862bf3fd9d027ef17170
jreyes0431 <jose-31.31@hotmail.com>
2025-05-04 22:28:01 -0500

    Adding tailwind config file

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    theme: {
+        extend: {
+            colors: {
+                burdeos: '#8B2C3B',
+                amarillo: '#FFD600',
+                negroprofundo: '#181818',
+                lilaclaro: '#B8A9C9',
+            },
+        },
+    },
+}
